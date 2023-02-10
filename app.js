77a975daa0307174538084cef4831a63a3057fee
Neul Seo <55679046+neulseo2@users.noreply.github.com>
2023-02-10 21:04:19 +0900

    added onLoginSubmit

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const loginInput = document.querySelector("#login-form input");
 // const loginInput = document.querySelector("#login-form input");
 
 function onLoginSubmit() {
-  event.preventDefault();
-  console.log(event);
+  const username = loginInput.value;
+  console.log(username);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
