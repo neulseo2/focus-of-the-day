ae1a77afe0e4d0b052cc7bea8d2b431514201d00
Neul Seo <55679046+neulseo2@users.noreply.github.com>
2023-02-26 10:53:13 -0500

    added deleteToDo

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,30 @@
 const todoForm = document.getElementById("todo-form");
-const toDoInput = document.getElementById("#todo-form input");
+const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
  
+function deleteToDo(event) {
+    const parentLi = event.target.parentElement;
+    parentLi.remove();
+}
+
+function paintToDo(newToDo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newToDo; // changing the text of the span
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo)
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
 
 function handleToDoSubmit(event) {
-    event.preventDefault;
-    console.log(toDoInput.value);
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = "";
+    paintToDo(newToDo);
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
